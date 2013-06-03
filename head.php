177979249fd70e67356b6dd2d137c8b7dbf2547d
wuzeyou <wuzeyouttsz@gmail.com>
2013-06-03 18:05:05 +0800

    Update head.php

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html">
 <title></title>
-<link href="images/css.css" rel="stylesheet" type="text/css">
+<link href="css.css" rel="stylesheet" type="text/css">
 <b><a href="add.php">Add Message</a> | <a href="list.php">View Messages</a>  | <a href="login.php">Login</a> </b>
 <hr size=1>
