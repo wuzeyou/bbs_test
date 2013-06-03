fb4c1466b5877bd05436459c4ffec2ba1f9949f6
wuzeyou <wuzeyouttsz@gmail.com>
2013-06-03 18:02:51 +0800

    Create login.php
    登录页，可以忽略

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+
+include("conn.php");
+
+  if($_GET[out]){
+    setcookie("cookie", "out");
+    echo "<script language=\"javascript\">location.href='login.php';</script>";
+  }
+
+
+  if($_POST[id]=='admin'){
+    $pw=md5($_POST[pw]);
+    if($pw=='e1bfd762321e409cee4ac0b6e841963c'){
+     setcookie("cookie", "ok");
+       echo "<script language=\"javascript\">location.href='login.php';</script>";
+    }
+  }
+include("head.php");
+if($_COOKIE['cookie']!='ok'){
+?>
+
+<SCRIPT language=javascript>
+function Checklogin()
+{
+  if (myform.id.value=="")
+  {
+    alert("Please input username");
+    myform.id.focus();
+    return false;
+  }
+    if (myform.pw.value=="")
+  {
+    alert("Password can't be empty");
+    myform.pw.focus();
+    return false;
+  }
+}
+</SCRIPT>
+
+<form action="" method="post" name="myform" onsubmit="return Checklogin();">
+  ID:<input type="text" name="id" /><br>
+  PW:<input type="password" name="pw" /> <input type="submit" name="submit" value="Login"/>
+  </form>
+<?php
+}else{
+?>
+  <a href='?out=login'>Logout</a>
+<?php
+}
+?>
