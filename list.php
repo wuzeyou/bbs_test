be3708edfd76f92f8e7187fd18b03dcf81f0f499
wuzeyou <wuzeyouttsz@gmail.com>
2013-06-03 18:02:14 +0800

    Create list.php
    留言展示页面

---
+++ list.php
@@ -0,0 +1,23 @@
+<?php
+
+ include("conn.php");
+
+ include("head.php");
+  $SQL="SELECT * FROM `message` order by id desc";
+  $query=mysql_query($SQL);
+  while($row=mysql_fetch_array($query)){  //依次列出所有留言
+?>
+
+<table width=500 border="0" cellpadding="5" cellspacing="1" bgcolor="#add3ef">
+  <tr bgcolor="#eff3ff">
+  <td>Title:<?=$row[title]?> User:<?=$row[user]?></td>
+  </tr>
+  <tr bgColor="#ffffff">
+  <td>Content:<?
+ echo htmtocode($row[content]);
+   ?></td>
+  </tr>
+</table>
+<?php
+  }
+?>
