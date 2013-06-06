865be26e76b649cfa917b286b88bec063b2fbf59
wuzeyou <wuzeyouttsz@gmail.com>
2013-06-06 20:00:38 +0800

    修改页面Title为BBS_test

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-<title></title>
+<title>BBS_test</title>
 <link href="images/css.css" rel="stylesheet" type="text/css">
 <b><a href="index.php">添加留言</a> | <a href="list.php">浏览留言</a>  | <a href="login.php">登陆</a> </b>
 <hr size=1>
